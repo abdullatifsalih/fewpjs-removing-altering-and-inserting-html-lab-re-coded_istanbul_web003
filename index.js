61fe71088922f14d365784b535be533642d023aa
abdullatifsalih <abdullatifsalih91@gmail.com>
2020-07-20 04:26:12 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let main = document.querySelector('main');
+main.remove();
+let element = document.createElement('h1');
+element.id='victory';
+document.body.appendChild(element);
+let newHeader = document.querySelector('h1#victory');
+newHeader.innerHTML='latif is the champion ';
